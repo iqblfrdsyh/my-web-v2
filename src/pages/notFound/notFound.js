ba584ab0177d79c40f8bbbbe461be931083bb319
iqblfrdsyh <iqblfrdsyh@gmail.com>
2023-01-30 19:38:09 +0700

    terbaru

---
+++ src/pages/notFound/notFound.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+    return (
+        <div style={{ margin: "150px auto", fontSize: "45px" }}>
+            <h1>Page Not Found</h1>
+        </div>
+    )
+}
+
+export default NotFound
--- src/pages/route.js
+++ src/pages/route.js
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './home'
+import NotFound from './notFound/notFound'
 import SecretoChat from './secretochat/secretochat'
-import ImageUpload from './testFoto/test'
+
 
 const Routers = () => {
     return (
@@ -9,7 +10,7 @@ const Routers = () => {
             <Routes>
                 <Route path='/' element={<Home />}></Route>
                 <Route path='/secretochat' element={<SecretoChat />}></Route>
-                <Route path='/a' element={<ImageUpload />}></Route>
+                <Route path='*' element={<NotFound />}></Route>
             </Routes>
         </BrowserRouter>
     )
